ad688c2a1ba0274707c3a6a0c710237cb7810910
HML786 <haanilalji2@gmail.com>
2025-10-25 11:50:04 -0400

    Create classes for db schema

---
+++ frontend/fridgemagnets/lib/models/magnet.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+class Magnet {
+  final int id;
+  final String? userId; // the user who placed the magnet
+  final String? text;
+  final Color? color; // from hex code
+  final double? x;
+  final double? y;
+  final String? imageUrl;
+
+  const Magnet({
+    required this.id,
+    required this.userId,
+    required this.text,
+    required this.color,
+    required this.x,
+    required this.y,
+    required this.imageUrl,
+  });
+}
