6e12592c763c102acbe1718cb401aef8e6d5c25b
HML786 <haanilalji2@gmail.com>
2025-10-25 12:07:32 -0400

    Start Home page and use Home page as home in main

---
+++ frontend/fridgemagnets/lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.account_circle)
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            "Fridges list here"
+          ),
+        )
+      )
+    );
+  }
+}
