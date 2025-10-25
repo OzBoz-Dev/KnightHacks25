1209fd6dd2a7a06aa4063d05e1e51681346aba5e
HML786 <haanilalji2@gmail.com>
2025-10-25 12:47:10 -0400

    Create Fridge Page to interact with a Fridge

---
+++ frontend/fridgemagnets/lib/pages/fridge_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FridgePage extends StatefulWidget {
+  const FridgePage({super.key});
+
+  @override
+  State<FridgePage> createState() => _FridgePageState();
+}
+
+class _FridgePageState extends State<FridgePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
