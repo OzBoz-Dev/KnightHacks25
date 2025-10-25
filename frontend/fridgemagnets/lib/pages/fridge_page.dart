0bd9d7020f6288e971726a7a165b61227a5bd328
HML786 <haanilalji2@gmail.com>
2025-10-25 13:31:14 -0400

    Start adding modal bottom sheet for storing magnets

--- frontend/fridgemagnets/lib/pages/fridge_page.dart
+++ frontend/fridgemagnets/lib/pages/fridge_page.dart
@@ -13,7 +13,47 @@ class _FridgePageState extends State<FridgePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.fridgeName),),
-      body: const Placeholder(),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.blue[500]),
+              ),
+              onPressed: () {
+                showModalBottomSheet(
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (context) {
+                    return SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          "Magnets list here",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue[500],
+                          ),
+                        ),
+                      )
+                    );
+                  }
+                );
+              },
+              label: Text(
+                "Open Magnets",
+                style: TextStyle(
+                  color: Colors.white
+                ),
+              ),
+              icon: Icon(Icons.arrow_drop_up, color: Colors.white,),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
